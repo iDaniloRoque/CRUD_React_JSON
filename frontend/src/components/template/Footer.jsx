3aa58cd178c6d5568304a3d75f6dbf0b917b15cf
Danilo-Roque <dsr.proj3ct@gmail.com>
2022-02-24 17:00:28 -0300

    Montando o rodapé

--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props => 
     <footer className="footer">
-        Rodapé
+        <span className="span">
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong> Danilo Roque</strong>
+        </span>
     </footer>

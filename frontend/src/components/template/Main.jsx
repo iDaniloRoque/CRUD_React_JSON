81f0d569410937115e85982aa80c149568fdf3ab
Danilo-Roque <dsr.proj3ct@gmail.com>
2022-02-25 08:45:04 -0300

    Ajustando Conteudo

--- frontend/src/components/template/Main.jsx
+++ frontend/src/components/template/Main.jsx
@@ -5,7 +5,10 @@ import React from 'react'
 export default props =>
 <React.Fragment>
     <Header {...props}/>
-        <main className="content">
-            Conteúdo
+        <main className="content container-fluid">
+            {/*div.p-3.mt-3*/}
+            <div className="p-3 mt-3">
+                {props.children}
+            </div>
         </main>
 </React.Fragment>
